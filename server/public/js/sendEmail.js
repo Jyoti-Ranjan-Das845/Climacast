32c5915d1b21370b02c0e7383e99024b70138d70
Jyoti Ranjan Das <jyotiranjandas@Jyotis-MacBook-Air.local>
2024-06-03 21:13:07 +0530

    Weather App

---
+++ server/public/js/sendEmail.js
@@ -0,0 +1,33 @@
+import nodemailer from 'nodemailer';
+
+// const nn = require('.')
+
+const sendEmail = async (notif, to, subject, text, link) => {
+	try {
+		const transporter = nodemailer.createTransport({
+			host : 'smtp.gmail.com',
+            service : 'gmail',
+			port: 587,
+			secure: true,
+			auth: {
+				user: 'jyoti.r.das845@gmail.com',
+				pass: 'vdyi deih fupp qxgj',
+			},
+		});
+
+		await transporter.sendMail({
+			from: `"${notif}" <jyoti.r.das845@gmail.com>`,
+			to: to,
+			subject: subject,
+			text: text,
+            html : `Click on this link to login <a href="${link}">login</a>`
+		});
+		console.log("email sent successfully");
+	} catch (error) {
+		console.log("email not sent!");
+		console.log(error);
+		return error;
+	}
+};
+
+export default sendEmail;
